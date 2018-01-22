06fa9761c24838bda946ec0b02c97af2f1a4f006
Alli Tzadok <allisontzadok@gmail.com>
2018-01-22 22:44:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 class Dog
   @@all = []
+  attr_accessor :name
 
   def self.clear_all
     @@all.clear
   end
-  
+
+  def self.all
+    @@all.each do |song|
+      puts song.name
+    end
+  end
+
 
 end
